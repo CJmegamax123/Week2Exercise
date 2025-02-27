b369b0fe79860622044ddf667c3329e8cb509d33
Carlos Jamerson Jr. <CCJJ2000@yahoo.com>
2025-02-27 04:04:53 -0600

    1st attempt

---
+++ Conductor.js
@@ -0,0 +1,6 @@
+document.getElementsById("galaxy").addEventListener("click", function() {
+    document.getElementById("bigPictureImage").src = "assets/galaxy.jpg"} );
+document.getElementsById("space").addEventListener("click", function() {
+    document.getElementById("bigPictureImage").src = "assets/space.jpg"});
+document.getElementsById("earth").addEventListener("click", function() {
+    document.getElementById("bigPictureImage").src = "assets/earth.jpg"});
